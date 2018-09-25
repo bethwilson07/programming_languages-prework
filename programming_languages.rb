3539c4e0d57aa57c6cd1c6ccfe81f94584fbe7a3
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-09-25 20:19:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,5 +1,6 @@
 def reformat_languages(languages)
   new_hash = {}
+  
   languages.each do |oo_or_functional, language_hash|
     language_hash.each do |language, attribute_hash|
       attribute_hash.each do |attribute, value|
